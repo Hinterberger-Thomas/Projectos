59644d49b4b33a4c637a2052fafa85532ea580f1
Hinterberger-Thomas <thomas.hinterberger.eu@gmail.com>
2021-05-04 15:26:19 +0100

    creaded Login

---
+++ src/components/Login.js
@@ -0,0 +1,10 @@
+const login = () => {
+    return (
+        <div>
+            <input type = "text"></input>
+            <button>click me</button>
+        </div>
+    )
+}
+
+export default login
